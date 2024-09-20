8a7b8c4c94da26ee5bfab305e0feda4ccf3a9e76
Silas Sefas <silassefas23@gmail.com>
2024-09-20 15:57:38 -0300

    Service layer, component registration

---
+++ src/main/java/com/silassefas/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.silassefas.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.silassefas.course.entities.User;
+import com.silassefas.course.repositories.UserRepository;
+
+@Service // registra a classe como serviço do spring, permitindo a injeção de dependencia
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User finById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
