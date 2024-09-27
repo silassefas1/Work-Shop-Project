5f4d703ae4762d777562e5088efa7ec4291028fd
Silas Sefas <silassefas23@gmail.com>
2024-09-27 11:22:40 -0300

    OrderStatus enum

--- src/main/java/com/silassefas/course/config/TestConfig.java
+++ src/main/java/com/silassefas/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.silassefas.course.entities.Order;
 import com.silassefas.course.entities.User;
+import com.silassefas.course.entities.enums.OrderStatus;
 import com.silassefas.course.repositories.OrderRepository;
 import com.silassefas.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner{
 		User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1); 
-		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2); 
-		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1); 
+		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2); 
+		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 		
 		userRepository.saveAll(Arrays.asList(user1,user2));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
