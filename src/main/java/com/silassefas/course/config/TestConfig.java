0bc1a7b7f0300afc418a923a5aa11be9b2610d21
Silas Sefas <silassefas23@gmail.com>
2024-10-14 09:12:18 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/silassefas/course/config/TestConfig.java
+++ src/main/java/com/silassefas/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.silassefas.course.entities.Category;
 import com.silassefas.course.entities.Order;
+import com.silassefas.course.entities.OrderItem;
 import com.silassefas.course.entities.Product;
 import com.silassefas.course.entities.User;
 import com.silassefas.course.entities.enums.OrderStatus;
 import com.silassefas.course.repositories.CategoryRepository;
+import com.silassefas.course.repositories.OrderItemRepository;
 import com.silassefas.course.repositories.OrderRepository;
 import com.silassefas.course.repositories.ProductRepository;
 import com.silassefas.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -48,25 +53,32 @@ public class TestConfig implements CommandLineRunner{
 		Category category2 = new Category(null, "Books"); 
 		Category category3 = new Category(null, "Computers"); 
 		
-		Product produt1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, ""); 
-		Product produt2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, ""); 
-		Product produt3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, ""); 
-		Product produt4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""); 
-		Product produt5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+		Product product1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, ""); 
+		Product product2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, ""); 
+		Product product3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, ""); 
+		Product product4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""); 
+		Product product5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
 		
 		userRepository.saveAll(Arrays.asList(user1,user2));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
 		categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
-		productRepository.saveAll(Arrays.asList(produt1, produt2, produt3, produt4, produt5));
+		productRepository.saveAll(Arrays.asList(product1, product2, product3, product4, product5));
 		
-		produt1.getCategories().add(category2);
-		produt2.getCategories().add(category1);
-		produt2.getCategories().add(category3);
-		produt3.getCategories().add(category3);
-		produt4.getCategories().add(category3);
-		produt5.getCategories().add(category2);
+		product1.getCategories().add(category2);
+		product2.getCategories().add(category1);
+		product2.getCategories().add(category3);
+		product3.getCategories().add(category3);
+		product4.getCategories().add(category3);
+		product5.getCategories().add(category2);
 		
-		productRepository.saveAll(Arrays.asList(produt1,produt2,produt3,produt4,produt5));
+		productRepository.saveAll(Arrays.asList(product1,product2,product3,product4,product5));
+		
+		OrderItem orderItem1 = new OrderItem(order1, product1, 2, product3.getPrice());
+		OrderItem orderItem2 = new OrderItem(order1, product3, 1, product3.getPrice());
+		OrderItem orderItem3 = new OrderItem(order2, product3, 2, product3.getPrice());
+		OrderItem orderItem4 = new OrderItem(order3, product5, 2, product5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(orderItem1, orderItem2, orderItem3, orderItem4));
 		
 		
 	}
