346beb2fb3e73c65e174cecdf7e31c242b80967d
Silas Sefas <silassefas23@gmail.com>
2024-10-29 15:34:25 -0300

    exception handling - delete and POM.mxl update

--- src/main/java/com/silassefas/course/services/UserService.java
+++ src/main/java/com/silassefas/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.silassefas.course.entities.User;
 import com.silassefas.course.repositories.UserRepository;
+import com.silassefas.course.services.exceptions.DatabaseException;
 import com.silassefas.course.services.exceptions.ResourceNotFoundException;
 
 @Service // registra a classe como serviço do spring, permitindo a injeção de dependencia
@@ -30,7 +33,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User object) {
